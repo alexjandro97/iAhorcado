9248198d42e89fa6f0e1ade32d3c6456e835b9ca
CETYS <cetys@cetyss-mac.local>
2017-10-17 10:08:00 +0200

    Tengo la vista acabada, he empezado con el controlador, imprimo las letras en en el Label y tengo dos variables, acierto y fallo inicializadas a 0

--- iAhorcado/ViewController.swift
+++ iAhorcado/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let acierto = 0
+    let fallo = 0
+    
+    
+    @IBOutlet var palabraOculta: UILabel!
+    
+    
+    @IBAction func letraPulsada(_ sender: UIButton) {
+        let letra : String! = sender.currentTitle
+        palabraOculta.text! += letra
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
